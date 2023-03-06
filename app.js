b2e558b85f117655460dbb5b422ae3b763daf78b
Oleksii <oleksiibuntikov5@gmail.com>
2023-03-06 09:39:19 +0200

    fixed bugs

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-let arr = [1, 2, 3, 4, false, 'string', 7, undefined, 8, 9];
-let newArr = [];
-for (let i = 0; i < arr.length; i++) {
-    if (typeof arr[i] !== Number) {
-         newArr.push(arr[i]);
-         console.log(newArr)
-
+let arr = [1, 2, 3, 4, 12, 'string', 7, undefined, 8, 9];
+let min = arr[0];
+let max = min;
+let sum = 0;
+for (let i = 0; i < arr.length; ++i) {
+    if (typeof arr[i] === "number") {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+        sum = sum + arr[i];
     }
-
 }
+console.log(max);
+console.log(min);
+console.log(sum);
